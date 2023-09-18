768fe0ed2dd255e4e6f962f57e014347ff988627
Xyfalix <nicklimcy90@gmail.com>
2023-09-18 22:53:58 +0800

    Refined search function and added error messages for invalid search and no returned results

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,8 @@ function App() {
   const [northeastLat, setNortheastLat] = useState(null);
   const [northeastLng, setNortheastLng] = useState(null);
   const [places, setPlaces] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
 
   const handleSearch = (newSearchCity) => {
     setSearchCity(newSearchCity);
@@ -27,19 +29,38 @@ function App() {
   useEffect(() => {
     async function fetchCoordinates() {
       if (searchCity) {
+        // Reset the following parameters everytime a new search is executed
+        setIsLoading(true);
+        setPlaces(null);
+        setIsError(false);
+        setSouthwestLat(null);
+        setSouthwestLng(null);
+        setNortheastLat(null);
+        setNortheastLng(null);
+  
         const apiUrl = `https://api.opencagedata.com/geocode/v1/json?q=${searchCity}&key=${openCageApiKey}&limit=1`
         const response = await fetch(apiUrl);
         const responseData = await response.json();
-        const bounds = responseData.results[0].bounds;
-        console.log(bounds)
-        setSouthwestLat(bounds.southwest.lat);
-        setSouthwestLng(bounds.southwest.lng);
-        setNortheastLat(bounds.northeast.lat);
-        setNortheastLng(bounds.northeast.lng);
-
-        console.log(`SouthWest Lat: ${southwestLat}, SouthWest Lng: ${southwestLng}, NorthEast Lat: ${northeastLat}, NorthEast Lng: ${northeastLng}`)
+        console.log(responseData)
+        // Set coordinates if bounds is defined
+        if (responseData.results.length > 0) {
+          const bounds = responseData.results[0].bounds;
+          console.log(bounds)
+          setSouthwestLat(bounds.southwest.lat);
+          setSouthwestLng(bounds.southwest.lng);
+          setNortheastLat(bounds.northeast.lat);
+          setNortheastLng(bounds.northeast.lng);
+        // otherwise return an error to the user.
+        } else {
+          setIsError(true);
+        }
       }
     }
+  
+    fetchCoordinates();
+  }, [searchCity]);
+  
+  useEffect(() => {
     async function fetchPlaces() {
       if (southwestLng && southwestLat && northeastLng && northeastLat) {
         const placesApiUrl = `https://api.geoapify.com/v2/places?categories=tourism,natural&filter=rect:${southwestLng},${southwestLat},${northeastLng},${northeastLat}&limit=20&apiKey=${geoApifyApiKey}`;
@@ -47,13 +68,32 @@ function App() {
         const placesResponseData = await placesResponse.json();
         console.log(placesResponseData);
         setPlaces(placesResponseData.features);
+        setIsLoading(false);
       }
     }
-    
-    fetchCoordinates();
+  
     fetchPlaces();
+    
+  }, [southwestLng, southwestLat, northeastLng, northeastLat]);
 
-  }, [searchCity, southwestLng, southwestLat, northeastLng, northeastLat,]);
+  let content = null;
+
+  // user has not started searching yet
+  if (!searchCity) {
+    content = <p>No searches executed yet</p>;
+  // user keyed in an invalid destination
+  } else if (isLoading && isError) {
+    content = <p>You keyed in an invalid destination, please try again.</p>
+  // user keyed in a valid destination and fetchPlaces is running
+  } else if (isLoading) {
+    content = <p>Loading your search results...</p>;
+  // fetchPlaces has completed running, results displayed
+  } else if (places.length > 0) {
+    content = <DestinationList places={places} />;
+  // no features were returned from fetchPlaces
+  } else {
+    content = <p>Sorry, no places were found based on your search terms, please try again.</p>;
+  }
 
   return (
     <>
@@ -61,11 +101,7 @@ function App() {
       <SearchCityInput onSearch={handleSearch}/>
       <button>View Bucket List</button>
       <h2>Search Results</h2>
-      {places ? (
-        <DestinationList places={places} />
-      ) : (
-        <p>Loading...</p>
-      )}
+      {content}
     </>
   )
 }
